ad7153482c3d587afa72e86f864b223ad86253ed
Boa <boa.matule@gmail.com>
2020-08-29 19:11:04 +0200

    Rewrites the upvote endpoint.

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ app.get('/api/articles/:name', async (req, res) => {
     const db = client.db('raw-details');
   
     const articleInfo = await db.collection('articles').findOne({name: articleName});
-    rest.status(200).json(articleInfo);
+    res.status(200).json(articleInfo);
   
     client.close(); 
     } catch (error) {
@@ -24,18 +24,35 @@ app.get('/api/articles/:name', async (req, res) => {
   }
 });
 
-app.post('/api/articles/:name/upvote', (req, res) => {
+app.post('/api/articles/:name/upvote', async (req, res) => {
+
+  try {
   const articleName = req.params.name;
 
-  articlesInfo[articleName].upvotes += 1;
-  res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`)
+  const client = await MongoClient.connect('mongodb://localhost:27017', {  useUnifiedTopology: true });
+  const db = client.db('raw-details');
+
+  const articleInfo = await db.collection('articles').findOne({ name: articleName});
+  await db.collection('articles').updateOne({ name: articleName}, {
+    '$set': {
+      upvotes: articleInfo.upvotes + 1, 
+    },
+  });
+  const updateArticleInfo = await db.collection('articles').findOne({ name: articleName}); 
+
+  res.status(200).json(updateArticleInfo);
+
+  client.close();
+  } catch (error) {
+    res.status(500).json({ message: 'Error connecting to db', error });
+  }
 });
 
 app.post('/api/articles/:name/add-comment', (req, res) => {
   const { username, text } = req.body;
   const articleName = req.params.name;
 
-  articlesInfo[articleName].comments.push({ username, text });
+  articleInfo[articleName].comments.push({ username, text });
   res.status(200).send(articlesInfo[articleName])  
 });
 
